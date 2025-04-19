0a8e3b0e2de80e4f3c1c716dc5536980bcb13e17
Your Name <you@example.com>
2025-04-19 22:26:04 +0600

    fix: Improve code formatting and readability in Root component

--- src/Page/Root/Root.jsx
+++ src/Page/Root/Root.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import Navbar from '../../Components/Navbar/Navbar';
-import { Outlet, useLocation } from 'react-router';
-import Footer from '../../Components/Footer/Footer';
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import { Outlet, useLocation } from "react-router";
+import Footer from "../../Components/Footer/Footer";
 
 const Root = () => {
-    const location=useLocation()
-    const isHomePage=location.pathname ==="/"
-    return (
-        <div className='font-sora max-w-7xl mx-auto'>
-            {
-                isHomePage?'':<Navbar></Navbar>
-            }
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+  return (
+    <div className="font-sora max-w-7xl mx-auto">
+      {!isHomePage && <Navbar></Navbar>}
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
