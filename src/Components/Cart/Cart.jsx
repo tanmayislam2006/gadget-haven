0945a6bd0392b202c287adc30c8a88fc8e468796
Your Name <you@example.com>
2025-04-19 12:34:26 +0600

    feat: Update daisyui dependency to version 5.0.27; enhance Cart component with purchase modal and improve item removal logic

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,11 +2,9 @@ import React, { use, useEffect, useState } from "react";
 import HeroImage from "../../assets/banner.jpg";
 import { getAddItem, removeItem } from "../../Utilities/Utilities";
 import { AllDataForDashboardContext } from "../../Page/DashBoard/DasBoard";
-import {  useNavigate } from "react-router";
+import PaymentIcon from "../../assets/Group.png";
 
 const Cart = () => {
-
-  const nevigate=useNavigate("")
   const [cartProduct, setCartProduct] = useState([]);
   const allProductsData = use(AllDataForDashboardContext);
   const cartProductFromLocalStorage = getAddItem("cartItems");
@@ -19,12 +17,16 @@ const Cart = () => {
   // Use state to manage the cart products
 
   const handleRemoveCart = (productId) => {
-
     removeItem(productId);
-    const remaingProducts=cartProduct.filter((item) => item.product_id !== productId)
+    const remaingProducts = cartProduct.filter(
+      (item) => item.product_id !== productId
+    );
     setCartProduct(remaingProducts);
   };
+const handlePurchase=()=>{
+  document.getElementById("my_modal_3").showModal()
 
+}
   return (
     <div className="my-5 mx-4">
       <div className="flex justify-between items-center my-4">
@@ -41,7 +43,10 @@ const Cart = () => {
           <button className="mx-5 border-2 rounded-4xl px-4 py-2 text-purple-600 font-bold ">
             Sort By Price
           </button>
-          <button onClick={()=>nevigate('purchase')} className="px-4 py-2 rounded-3xl bg-purple-600 font-bold text-white cursor-pointer mx-5">
+          <button
+            onClick={handlePurchase}
+            className="px-4 py-2 rounded-3xl bg-purple-600 font-bold text-white cursor-pointer mx-5"
+          >
             Purchase
           </button>
         </div>
@@ -75,6 +80,35 @@ const Cart = () => {
           );
         })}
       </div>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <div className="">
+            <div className="flex justify-center py-6">
+              <img src={PaymentIcon} alt="" />
+            </div>
+            <div className="flex flex-col items-center my-3">
+              <h1 className="font-bold text-2xl text-center">
+                Payment Successfully
+              </h1>
+              <p className="text-gray-600 text-center font-semibold my-1">
+                Thanks for purchasing.
+              </p>
+              <p className="text-gray-600 text-center font-semibold my-1">
+                Total:{" "}
+                {cartProduct
+                  .reduce((total, item) => total + parseInt(item.price), 0)
+                  .toFixed(2)}
+              </p>
+            </div>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
